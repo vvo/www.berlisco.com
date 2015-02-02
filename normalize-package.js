dc6bf07924088f1cd2df9da6fd717d93257668fa
vvo <vincent@zeroload.net>
2015-02-02 23:46:29 +0100

    remove uneeded infos from packages
    licenses, dependencies, devDependencies, releases are not needed
    releasesCount, dependenciesCount are needed

--- normalize-package.js
+++ normalize-package.js
@@ -1,23 +1,35 @@
-var normalize = require('npm-registry/normalize').packages;
-
 module.exports = normalizePackage;
 
+var debug = require('debug')('berlisco:normalize-package');
+var normalize = require('npm-registry/normalize').packages;
+
 function normalizePackage(pkg) {
-  pkg = normalize(pkg);
-  return {
-    name: pkg.name,
-    description: pkg.description,
-    keywords: pkg.keywords,
-    author: pkg._npmUser,
-    licenses: pkg.licenses,
-    github: pkg.github,
-    maintainers: pkg.maintainers,
-    dependencies: pkg.dependencies,
-    devDependencies: pkg.devDependencies,
-    version: pkg.latest.version,
-    releases: pkg.releases,
-    bin: pkg.bin,
-    created: pkg.created,
-    modified: pkg.modified
+  var normalized = normalize(pkg);
+
+  var cleaned = {
+    name: normalized.name,
+    description: normalized.description,
+    keywords: normalized.keywords,
+
+    // author: an object like
+    // "author":{"name":"tmpvar","email":"tmpvar@gmail.com","gravatar_id":"..","gravatar":".."}
+    author: normalized.maintainers[0],
+
+    // github:
+    // "github":{"user":"tmpvar","repo":"polygon.clip.js"}
+    github: normalized.github,
+
+    // array of authors
+    maintainers: normalized.maintainers,
+
+    version: normalized.latest.version,
+    dependenciesCount: Object.keys(normalized.dependencies).length,
+    releasesCount: Object.keys(normalized.releases).length,
+    bin: normalized.bin,
+    created: normalized.created,
+    modified: normalized.modified
   };
+
+  debug('normalized package to %j', cleaned);
+  return cleaned;
 }
